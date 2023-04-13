22b321103bbc2092ab9cd3e73e454ce40cdf000b
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-04-13 16:34:25 +0000

    Add refresh_policies.py to get all system-defined policies by API call

---
+++ scripts/refresh_policies.py
@@ -0,0 +1,69 @@
+import requests
+import json
+from authentication import authenticate
+
+
+def get_iam_policies():
+    endpoint = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3"
+    
+    # Get User token
+    token = authenticate()
+
+    roles_headers = {
+        'X-Auth-Token': token,
+        'X-Subject-Token': token
+    }
+    response = requests.request(
+        "GET", f"{endpoint}/roles", headers=roles_headers, data={})
+
+    # Loading IAM policies in JSON
+    policies = response.json()
+
+    # Filtering IAM policies
+    roles_list = []
+    for role in policies['roles']:
+        role_dict = {
+            'id': role['id'],
+        }
+        try:
+            role_dict['catalog'] = role['catalog']
+        except KeyError:
+            role_dict['catalog'] = None
+
+        try:
+            role_dict['description'] = role['description']
+        except KeyError:
+            role_dict['description'] = None
+
+        try:
+            role_dict['display_name'] = role['display_name']
+        except KeyError:
+            role_dict['display_name'] = None
+
+        try:
+            role_dict['policy'] = role['policy']
+        except KeyError:
+            role_dict['policy'] = None
+
+        roles_list.append(role_dict)
+
+    # Grouping Policies based on Service names (Catalog).
+    grouped_data = {}
+    for role in roles_list:
+        catalog = role.get('catalog')
+        if catalog:
+            if catalog not in grouped_data:
+                grouped_data[catalog] = []
+            grouped_data[catalog].append(role)
+
+    # Saving policies in policies.json file
+    try:
+        with open('policies.json', 'w') as f:
+            json.dump(grouped_data, f, indent=2, sort_keys=True)
+            print("policies.json was generated successfully!")
+    except IOError:
+        print("An error occurred while writing to the file 'policies.json'.")
+        exit(1)
+
+if __name__ == "__main__":
+    get_iam_policies()
