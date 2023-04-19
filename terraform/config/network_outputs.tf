61a39fcbd6f78f69aa136499322b1387da1cc0f3
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-04-19 22:17:48 +0000

    Add outputs.tf for Network tenant deployments

--- terraform/config/network_outputs.tf
+++ terraform/config/network_outputs.tf
@@ -1,19 +1,47 @@
-# VPC ID as output
+# VPCs outputs
 output "vpc_id" {
   value = module.network_vpc.vpc_id
 }
 
-# VPC name as output
 output "vpc_name" {
   value = module.network_vpc.vpc_name
 }
 
-# Subnet IDs as output
 output "subnet_ids" {
   value = module.network_vpc.subnet_ids
 }
 
-# Network IDs as output
 output "network_ids" {
   value = module.network_vpc.network_ids
 }
+
+# Security Group outputs
+output "secgroup_id" {
+  description = "The ID of the security group"
+  value       = module.sg_firewall.id
+}
+
+output "secgroup_name" {
+  description = "The name of the security group"
+  value       = module.sg_firewall.name
+}
+
+# KeyPair outputs
+output "keypair_id" {
+  description = "The ID of the KeyPair"
+  value       =  module.keypair.id
+}
+
+# EIP outputs
+output "firewall_public_ip" {
+  description = "firewall Elastic IP Address"
+  value       = module.firewall_eip.public_ip
+}
+
+output "firewall_eip_id" {
+  description = "firewall Elastic IP ID"
+  value       = module.firewall_eip.id
+}
+
+# Firewall instances outputs
+
