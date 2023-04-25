121199846af32c5131614ecd79296e15745ffcf2
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-04-25 15:59:57 +0000

    Add keypair name output

--- terraform/modules/keypair/outputs.tf
+++ terraform/modules/keypair/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "The ID of the KeyPair"
   value       = flexibleengine_compute_keypair_v2.keypair.id
 }
+
+output "name" {
+  description = "The name of the KeyPair"
+  value       = flexibleengine_compute_keypair_v2.keypair.name
+}
