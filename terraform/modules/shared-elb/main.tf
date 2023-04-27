ab439b47ad51d88a3a88226f30579121ae8cc6c4
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-04-27 23:55:49 +0000

    Update ELB arguments and vars to supports HTTPS, Certificate,.. cases

--- terraform/modules/shared-elb/main.tf
+++ terraform/modules/shared-elb/main.tf
@@ -11,21 +11,20 @@ terraform {
 
 resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer" {
   name          = var.loadbalancer_name
-  description =  var.description == "" ? "${var.loadbalancer_name} shared load balancer" : var.description
+  description   = var.description == "" ? "${var.loadbalancer_name} shared load balancer" : var.description
   vip_subnet_id = var.subnet_id
 
+  vip_address           = var.vip_address == "" ? null : var.vip_address
+  loadbalancer_provider = var.loadbalancer_provider == "" ? null : var.loadbalancer_provider
+  security_group_ids    = var.security_group_ids == [] ? null : var.security_group_ids
 
-  vip_address   = var.vip_address == "" ? null : var.vip_address
-  loadbalancer_provider   = var.loadbalancer_provider == "" ? null : var.loadbalancer_provider
-  security_group_ids = var.security_group_ids == [] ? null : var.security_group_ids
-  
-  admin_state_up = var.admin_state_up  // either true UP or false DOWN
-  
+  admin_state_up = var.admin_state_up // either true UP or false DOWN
+
   tags = var.tags
 }
 
 resource "flexibleengine_elb_certificate" "cert" {
-  count       = var.cert && var.certId == null ? 1 : 0
+  count = var.cert && var.certId == null ? 1 : 0
 
   name        = var.cert_name
   domain      = var.domain
@@ -34,54 +33,52 @@ resource "flexibleengine_elb_certificate" "cert" {
 }
 
 resource "flexibleengine_lb_listener_v2" "listeners" {
-  count                     = length(var.listeners)
+  count = length(var.listeners)
 
-  name                      = "${var.loadbalancer_name}-${element(var.listeners.*.name, count.index)}"
-  protocol                  = element(var.listeners.*.protocol, count.index)
-  protocol_port             = element(var.listeners.*.port, count.index)
-  loadbalancer_id           = flexibleengine_lb_loadbalancer_v2.loadbalancer.id
-  
+  name            = "${var.loadbalancer_name}-${element(var.listeners.*.name, count.index)}"
+  protocol        = element(var.listeners.*.protocol, count.index)
+  protocol_port   = element(var.listeners.*.port, count.index)
+  loadbalancer_id = flexibleengine_lb_loadbalancer_v2.loadbalancer.id
+
   default_tls_container_ref = var.cert && element(var.listeners.*.hasCert, count.index) && var.certId == null ? element(flexibleengine_elb_certificate.cert.*.id, 0) : var.cert && element(var.listeners.*.hasCert, count.index) && var.certId != null ? var.certId : null
-  description = element(var.listeners.*.description, count.index)  == "" ? null : element(var.listeners.*.description, count.index)
-  
-  // Optional
-  http2_enable                       = var.listeners[count.index].http2_enable == null ? null : element(var.listeners.*.http2_enable, count.index) && element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? var.listeners.*.http2_enable[count.index] : null
-  transparent_client_ip_enable       = var.listeners[count.index].transparent_client_ip_enable == null ? null : element(var.listeners.*.protocol, count.index) == "TCP" || element(var.listeners.*.protocol, count.index) == "UDP" ? element(var.listeners.*.transparent_client_ip_enable, count.index) : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? true : null
-  idle_timeout                       = var.listeners[count.index].idle_timeout == null ? null : element(var.listeners.*.idle_timeout, count.index) == "" ? null : element(var.listeners.*.idle_timeout, count.index)
-  request_timeout                    = var.listeners[count.index].request_timeout == null ? null : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.request_timeout, count.index), 60) : null
-  response_timeout                   = var.listeners[count.index].response_timeout == null ? null : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.response_timeout, count.index), 60) : null
-  tls_ciphers_policy                 = var.listeners[count.index].tls_ciphers_policy == null ? null : element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.tls_ciphers_policy, count.index), "tls-1-0") : null
+  description               = element(var.listeners.*.description, count.index) == "" ? null : element(var.listeners.*.description, count.index)
+
+  http2_enable                 = var.listeners[count.index].http2_enable == null ? null : element(var.listeners.*.http2_enable, count.index) && element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? var.listeners.*.http2_enable[count.index] : null
+  transparent_client_ip_enable = var.listeners[count.index].transparent_client_ip_enable == null ? null : element(var.listeners.*.protocol, count.index) == "TCP" || element(var.listeners.*.protocol, count.index) == "UDP" ? element(var.listeners.*.transparent_client_ip_enable, count.index) : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? true : null
+  idle_timeout                 = var.listeners[count.index].idle_timeout == null ? null : element(var.listeners.*.idle_timeout, count.index) == "" ? null : element(var.listeners.*.idle_timeout, count.index)
+  request_timeout              = var.listeners[count.index].request_timeout == null ? null : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.request_timeout, count.index), 60) : null
+  response_timeout             = var.listeners[count.index].response_timeout == null ? null : element(var.listeners.*.protocol, count.index) == "HTTP" || element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.response_timeout, count.index), 60) : null
+  tls_ciphers_policy           = var.listeners[count.index].tls_ciphers_policy == null ? null : element(var.listeners.*.protocol, count.index) == "TERMINATED_HTTPS" ? coalesce(element(var.listeners.*.tls_ciphers_policy, count.index), "tls-1-0") : null
 
   tags = element(var.listeners.*.tags, count.index) == {} ? null : element(var.listeners.*.tags, count.index)
-  
-  depends_on = [
-    flexibleengine_elb_certificate.cert
-  ]
+
+  //depends_on = [flexibleengine_elb_certificate.cert]
 }
 
 resource "flexibleengine_lb_pool_v2" "pools" {
-  count           = length(var.pools)
+  count = length(var.pools)
 
-  name            = element(var.pools.*.name, count.index)
-  protocol        = element(var.pools.*.protocol, count.index)
-  lb_method       = element(var.pools.*.lb_method, count.index)
-  
+  name      = element(var.pools.*.name, count.index)
+  protocol  = element(var.pools.*.protocol, count.index)
+  lb_method = element(var.pools.*.lb_method, count.index)
+
   listener_id     = var.pools[count.index].listener_index != null ? flexibleengine_lb_listener_v2.listeners[lookup(var.pools[count.index], "listener_index", count.index)].id : null
   loadbalancer_id = var.pools[count.index].listener_index == null ? flexibleengine_lb_loadbalancer_v2.loadbalancer.id : null
 
+  description    = element(var.pools.*.description, count.index) == null ? null : element(var.pools.*.description, count.index)
+  admin_state_up = element(var.pools.*.admin_state_up, count.index) == null ? null : element(var.pools.*.admin_state_up, count.index)
+
   /*
-  description = element(var.pools.*.desc, count.index) == "" ? null : element(var.pools.*.desc, count.index)
-  admin_state_up = element(var.pools.*.admin_state_up, count.index)
-  persistence = {
-    type        = element(var.pools.*.persistence.type, count.index) == "" ? null : element(var.pools.*.desc, count.index)
-    cookie_name = element(var.pools.*.persistence.cookie_name, count.index) == "" ? null : element(var.pools.*.desc, count.index)
-    timeout     = element(var.pools.*.persistence.timeout, count.index)
-  }
+  persistence = [{
+    type        = element(var.pools.*.type, count.index) == null ? null : element(var.pools.*.type, count.index)
+    cookie_name = element(var.pools.*.cookie_name, count.index) == null ? null : element(var.pools.*.cookie_name, count.index)
+    timeout     = element(var.pools.*.cookie_name, count.index) == "APP_COOKIE" ? null : element(var.pools.*.timeout, count.index) == null ? null : element(var.pools.*.timeout, count.index)
+  }]
   */
 }
 
 resource "flexibleengine_lb_member_v2" "members" {
-  count         = length(var.backends)
+  count = length(var.backends)
 
   name          = "${flexibleengine_lb_pool_v2.pools[lookup(var.backends[count.index], "pool_index", count.index)].name}-${element(var.backends.*.name, count.index)}"
   address       = var.backends_addresses[var.backends[count.index].address_index]
@@ -89,15 +86,14 @@ resource "flexibleengine_lb_member_v2" "members" {
   pool_id       = flexibleengine_lb_pool_v2.pools[lookup(var.backends[count.index], "pool_index", count.index)].id
   subnet_id     = var.backends[count.index].subnet_id
 
-  /*
-  weight = element(var.pools.*.weight, count.index) == 0 ? null : element(var.pools.*.weight, count.index)
-  admin_state_up = element(var.pools.*.admin_state_up, count.index) 
-  */
-  depends_on    = [flexibleengine_lb_pool_v2.pools]
+  weight         = element(var.backends.*.weight, count.index) == null ? null : element(var.backends.*.weight, count.index)
+  admin_state_up = element(var.backends.*.admin_state_up, count.index) == null ? null : element(var.backends.*.admin_state_up, count.index)
+
+  depends_on = [flexibleengine_lb_pool_v2.pools]
 }
 
 resource "flexibleengine_lb_monitor_v2" "monitor" {
-  count       = length(var.monitors)
+  count = length(var.monitors)
 
   name        = "${flexibleengine_lb_pool_v2.pools[lookup(var.monitors[count.index], "pool_index", count.index)].name}-${element(var.monitors.*.name, count.index)}"
   pool_id     = flexibleengine_lb_pool_v2.pools[lookup(var.monitors[count.index], "pool_index", count.index)].id
@@ -106,50 +102,49 @@ resource "flexibleengine_lb_monitor_v2" "monitor" {
   timeout     = var.monitors[count.index].timeout
   max_retries = var.monitors[count.index].max_retries
 
-  /*
-  url_path = var.monitors[count.index].url_path == "" ? null : element(var.monitors[count.index].url_path, count.index)
-  http_method = var.monitors[count.index].http_method == "" ? null : element(var.monitors[count.index].http_method, count.index)
-  expected_codes = var.monitors[count.index].expected_codes == "" ? null : element(var.monitors[count.index].expected_codes, count.index)
- 
-  admin_state_up = var.monitors[count.index].admin_state_up
-  */
-  depends_on  = [flexibleengine_lb_pool_v2.pools, flexibleengine_lb_member_v2.members]
+  url_path       = var.monitors[count.index].url_path == null ? null : var.monitors[count.index].protocol == "HTTP" || var.monitors[count.index].protocol == "HTTPS" ? element(var.monitors.*.url_path, count.index) : null
+  http_method    = var.monitors[count.index].http_method == null ? null : var.monitors[count.index].protocol == "HTTP" || var.monitors[count.index].protocol == "HTTPS" ? element(var.monitors.*.http_method, count.index) : null
+  expected_codes = var.monitors[count.index].expected_codes == null ? null : var.monitors[count.index].protocol == "HTTP" || var.monitors[count.index].protocol == "HTTPS" ? element(var.monitors.*.expected_codes, count.index) : null
+
+  admin_state_up = var.monitors[count.index].admin_state_up == "" ? null : var.monitors[count.index].admin_state_up
+
+  depends_on = [flexibleengine_lb_pool_v2.pools, flexibleengine_lb_member_v2.members]
 }
 
 
 resource "flexibleengine_lb_whitelist_v2" "whitelists" {
-  count            = length(var.listeners_whitelist)
+  count = length(var.listeners_whitelist)
 
-  listener_id      = flexibleengine_lb_listener_v2.listeners[lookup(var.listeners_whitelist[count.index], "listener_index", count.index)].id
-  
-  // Optional
-  enable_whitelist = var.listeners_whitelist[count.index].enable_whitelist
-  whitelist        = var.listeners_whitelist[count.index].whitelist == "" ? null : element(var.listeners_whitelist[count.index].whitelist, count.index)
+  listener_id = flexibleengine_lb_listener_v2.listeners[lookup(var.listeners_whitelist[count.index], "listener_index", count.index)].id
+
+  enable_whitelist = var.listeners_whitelist[count.index].enable_whitelist == null ? false : true
+  whitelist        = var.listeners_whitelist[count.index].whitelist == null ? null : element(var.listeners_whitelist[count.index].whitelist, count.index)
 }
 
 resource "flexibleengine_lb_l7policy_v2" "l7policies" {
-  count                = length(var.l7policies)
+  count = length(var.l7policies)
 
-  name                 = var.l7policies[count.index].name
-  action               = var.l7policies[count.index].action
-  listener_id          = flexibleengine_lb_listener_v2.listeners[lookup(var.l7policies[count.index], "listener_index", count.index)].id
+  name        = var.l7policies[count.index].name
+  action      = var.l7policies[count.index].action
+  listener_id = flexibleengine_lb_listener_v2.listeners[lookup(var.l7policies[count.index], "listener_index", count.index)].id
 
   redirect_listener_id = var.l7policies[count.index].redirect_listener_index != null ? flexibleengine_lb_listener_v2.listeners[lookup(var.l7policies[count.index], "redirect_listener_index", count.index)].id : null
   redirect_pool_id     = var.l7policies[count.index].redirect_pool_index != null ? flexibleengine_lb_pool_v2.pools[lookup(var.l7policies[count.index], "redirect_pool_index", count.index)].id : null
-  description          = var.l7policies[count.index].description  == "" ? null : element(var.l7policies[count.index].description, count.index)
 
-  //admin_state_up = var.l7policies[count.index].admin_state_up
+  description    = var.l7policies[count.index].description == null ? null : element(var.l7policies.*.description, count.index)
+  position       = var.l7policies[count.index].position == null ? null : var.l7policies[count.index].position
+  admin_state_up = var.l7policies[count.index].admin_state_up == null ? null : var.l7policies[count.index].admin_state_up
 }
 
 resource "flexibleengine_lb_l7rule_v2" "l7rules" {
-  count        = length(var.l7policies_rules)
+  count = length(var.l7policies_rules)
 
   l7policy_id  = flexibleengine_lb_l7policy_v2.l7policies[lookup(var.l7policies_rules[count.index], "l7policy_index", count.index)].id
   type         = var.l7policies_rules[count.index].type
   compare_type = var.l7policies_rules[count.index].compare_type
   value        = var.l7policies_rules[count.index].value
 
-  key          = var.l7policies_rules[count.index].key == "" ? null : element(var.l7policies_rules[count.index].key, count.index)
-  
-  //admin_state_up = var.l7policies_rules[count.index].admin_state_up
+  key = var.l7policies_rules[count.index].key == null ? null : element(var.l7policies_rules.*.key, count.index)
+
+  admin_state_up = var.l7policies_rules[count.index].admin_state_up == null ? null : var.l7policies_rules[count.index].admin_state_up
 }
--- terraform/modules/shared-elb/vars.tf
+++ terraform/modules/shared-elb/vars.tf
@@ -7,7 +7,7 @@ variable "loadbalancer_name" {
 variable "description" {
   description = "The description for the loadbalancer"
   type        = string
-  default = ""
+  default     = ""
 }
 
 variable "subnet_id" {
@@ -18,30 +18,30 @@ variable "subnet_id" {
 variable "vip_address" {
   description = "Address of the VIP (In the same Subnet)"
   type        = string
-  default = ""
+  default     = ""
 }
 
 variable "loadbalancer_provider" {
   description = "The name of the provider. Currently, only vlb is supported"
   type        = string
-  default = ""
+  default     = ""
 }
 
 variable "security_group_ids" {
   description = "A list of security group IDs to apply to the loadbalancer"
   type        = list(string)
-  default = []
+  default     = []
 }
 
 variable "admin_state_up" {
   description = "The administrative state of the loadbalancer. A valid value is true (UP) or false (DOWN)"
   type        = bool
-  default = true
+  default     = true
 }
 
 variable "tags" {
   description = "The key/value pairs to associate with the loadbalancer"
-  type = map(string)
+  type        = map(string)
   default = {
     Environment = "landing-zone"
   }
@@ -56,55 +56,54 @@ variable "cert" {
 
 variable "cert_name" {
   description = "the certifiacet name"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "certId" {
   description = "the certificate ID"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "private_key" {
   description = "the private key in string format"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "certificate" {
   description = "the certifiacate in string format"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "domain" {
   description = "Domain name"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 // listeners vars
 variable "listeners" {
   description = "Listeners list"
   type = list(object({
-    name     = string
-    port     = number
-    protocol = string #Protocol used TCP, UDP, HTTP or TERMINATED_HTTPS
-    hasCert  = bool
-    description  = string
-    
-    http2_enable = optional(bool)
+    name        = string
+    port        = number
+    protocol    = string #Protocol used TCP, UDP, HTTP or TERMINATED_HTTPS
+    hasCert     = bool
+    description = string
+
+    http2_enable                 = optional(bool)
     transparent_client_ip_enable = optional(bool)
-    idle_timeout = optional(number)
-    request_timeout = optional(number)
-    response_timeout = optional(number)
-    tls_ciphers_policy = optional(string)
-    
-    tags =  optional(map(string))
+    idle_timeout                 = optional(number)
+    request_timeout              = optional(number)
+    response_timeout             = optional(number)
+    tls_ciphers_policy           = optional(string)
+
+    tags = optional(map(string))
   }))
 }
-
 
 // pools vars
 variable "pools" {
@@ -115,6 +114,13 @@ variable "pools" {
     lb_method      = string # Load Balancing method (ROUND_ROBIN recommended)
     listener_index = number # Listenerused in this pool (Can be null)
 
+    description    = optional(string)
+    admin_state_up = optional(bool)
+
+    //type        = optional(string)
+    //cookie_name = optional(string)
+    // timeout parameter invalid if type is APP_COOKIE
+    //timeout     = optional(number)
   }))
 }
 
@@ -127,12 +133,15 @@ variable "backends" {
     address_index = string
     pool_index    = number
     subnet_id     = string
+
+    admin_state_up = optional(bool)
+    weight         = optional(number)
   }))
 }
 
 variable "backends_addresses" {
   description = "List of backends adresses"
-  type        = list
+  type        = list(any)
 }
 
 // monitors vars
@@ -145,9 +154,13 @@ variable "monitors" {
     delay       = number
     timeout     = number
     max_retries = number
-    //url_path       = string
-    //http_method    = string
-    //expected_codes = string
+
+    admin_state_up = optional(bool)
+
+    // required only when protocol is set to HTTP or HTTPS.
+    url_path       = optional(string)
+    http_method    = optional(string)
+    expected_codes = optional(string)
   }))
   default = []
 }
@@ -158,7 +171,8 @@ variable "listeners_whitelist" {
   type = list(object({
     listeners_index  = number
     enable_whitelist = bool
-    whitelist        = string #Comma separated : "192.168.11.1,192.168.0.1/24,192.168.201.18/8"
+
+    whitelist = optional(string) #Comma separated : "192.168.11.1,192.168.0.1/24,192.168.201.18/8"
   }))
   default = []
 }
@@ -167,13 +181,16 @@ variable "listeners_whitelist" {
 variable "l7policies" {
   description = "List of L7 policies redirected to pools/listeners"
   type = list(object({
-    name                    = string
-    action                  = string # REDIRECT_TO_POOL / REDIRECT_TO_LISTENER
-    description             = string
-    position                = number
-    listener_index          = number
+    name           = string
+    action         = string # REDIRECT_TO_POOL / REDIRECT_TO_LISTENER
+    description    = optional(string)
+    listener_index = number
+
     redirect_listener_index = number # if REDIRECT_TO_LISTENER is set, or null LISTENER must be listen on HTTPS_TERMINATED
     redirect_pool_index     = number # if REDIRECT_TO_POOL is set, or null - pool used to redirect must be not associated with a listener
+
+    position       = optional(number)
+    admin_state_up = optional(bool)
   }))
   default = []
 }
@@ -186,8 +203,12 @@ variable "l7policies_rules" { # Only if REDIRECT_TO_POOL
     type           = string
     compare_type   = string
     value          = string
-    description = string
-    key = string
+    description    = optional(string)
+
+    // Valid when type is set to COOKIE or HEADER. 
+    key = optional(string)
+
+    admin_state_up = optional(bool)
   }))
   default = []
 }
