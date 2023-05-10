28b4054571599c0aabcd2fb863e86f19d094b2fe
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-05-10 13:03:43 +0000

    Update AntiDDOS modules

--- terraform/modules/antiddos/main.tf
+++ terraform/modules/antiddos/main.tf
@@ -11,12 +11,12 @@ terraform {
 
 # Enable AntiDDOS on EIP
 resource "flexibleengine_antiddos_v1" "antiddos" {
-  count = length(var.eip_ids)
+  count = length(var.eips)
 
-  floating_ip_id         = var.eip_ids[count.index]
-  enable_l7              = true
-  traffic_pos_id         = 1
-  http_request_pos_id    = 3
-  cleaning_access_pos_id = 2
-  app_type_id            = 0
+  floating_ip_id         = var.eips[count.index].floating_ip_id
+  enable_l7              = var.eips[count.index].enable_l7
+  traffic_pos_id         = var.eips[count.index].traffic_pos_id
+  http_request_pos_id    = var.eips[count.index].http_request_pos_id
+  cleaning_access_pos_id = var.eips[count.index].cleaning_access_pos_id
+  app_type_id            = var.eips[count.index].app_type_id
 }
--- terraform/modules/antiddos/vars.tf
+++ terraform/modules/antiddos/vars.tf
@@ -1,5 +1,11 @@
-variable "eip_ids" {
-  description = "EIP IDs to enable protection"
-  type        = list(string)
-  default = ["ttssg","sfdf"]
+variable "eips" {
+  description = "Public IPs list"
+  type = list(object({
+    floating_ip_id        = string
+    enable_l7        = bool
+    traffic_pos_id    = number
+    http_request_pos_id     = number
+    cleaning_access_pos_id = number
+    app_type_id = number
+  }))
 }
