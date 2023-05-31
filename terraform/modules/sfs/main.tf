ab38192fb2c477aad4b04065d662107dac7def59
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-05-31 18:55:50 +0000

    Add SFS module, SFS file systems

--- terraform/modules/sfs/main.tf
+++ terraform/modules/sfs/main.tf
@@ -8,3 +8,21 @@ terraform {
     }
   }
 }
+
+resource "flexibleengine_sfs_file_system_v2" "sfs" {
+  count = length(var.sfs_shares)
+
+  name         = var.sfs_shares[count.index]["name"]
+  size         = var.sfs_shares[count.index]["size"]
+  share_proto  = var.sfs_shares[count.index]["share_protocol"] == null ? null : var.sfs_shares[count.index]["share_protocol"]
+  access_level = var.sfs_shares[count.index]["access_level"] == null ? null : var.sfs_shares[count.index]["access_level"]
+  access_to    = var.sfs_shares[count.index]["vpc_id"] == null ? null : var.sfs_shares[count.index]["vpc_id"]
+  description  = var.sfs_shares[count.index]["description"] == null ? null : var.sfs_shares[count.index]["description"]
+
+  metadata = {
+    "#sfs_crypt_key_id"    = var.sfs_shares[count.index]["kms_id"] == null ? null : var.sfs_shares[count.index]["kms_id"]
+    "#sfs_crypt_domain_id" = var.sfs_shares[count.index]["kms_domain_id"] == null ? null : var.sfs_shares[count.index]["kms_domain_id"]
+    "#sfs_crypt_alias"     = var.sfs_shares[count.index]["kms_key_alias"] == null ? null : var.sfs_shares[count.index]["kms_key_alias"]
+  }
+}
+
--- terraform/modules/sfs/vars.tf
+++ terraform/modules/sfs/vars.tf
@@ -1,1 +1,17 @@
-//
+// SFS variables
+variable "sfs_shares" {
+  description = "List of maps containing SFS Files systems"
+  type = list(object({
+    name      = string
+    size      = string
+    share_protocol    = optional(string)
+    access_level    = optional(string)
+    vpc_id    = optional(string)
+    description = optional(string)
+
+    kms_id    = optional(string)
+    kms_domain_id    = optional(string)
+    kms_key_alias = optional(string)
+  }))
+  default = []
+}
