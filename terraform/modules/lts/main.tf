8016c463b5f9e640e94d9252e9a71309dd0db694
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-06-06 14:35:27 +0000

    Init LTS module

--- terraform/modules/lts/main.tf
+++ terraform/modules/lts/main.tf
@@ -8,30 +8,3 @@ terraform {
     }
   }
 }
-
-resource "flexibleengine_ces_alarmrule" "alarm_rule" {
-  alarm_name = "alarm_rule"
-
-  metric {
-    namespace   = "SYS.ECS"
-    metric_name = "network_outgoing_bytes_rate_inband"
-    dimensions {
-        name  = "instance_id"
-        value = var.instance_id
-    }
-  }
-  condition  {
-    period              = 300
-    filter              = "average"
-    comparison_operator = ">"
-    value               = 6
-    unit                = "B/s"
-    count               = 1
-  }
-  alarm_actions {
-    type = "notification"
-    notification_list = [
-      var.topic_id
-    ]
-  }
-}
