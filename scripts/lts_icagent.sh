119eaf46a4172ead81141e92c296555093b405bf
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-07-06 23:03:41 +0000

    Add LTSAgent client script

---
+++ scripts/lts_icagent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install ICAgent client on Hosts to collect and send logs to LTS
+# Run this script on hosts.
+
+ak="AK_HERE"
+sk="SK_HERE"
+projectid="XXXXXXXXXXXX"
+region="eu-west-0"
+
+command="curl http://icagent-eu-west-0.oss.eu-west-0.prod-cloud-ocb.orange-business.com/ICAgent_linux/apm_agent_install.sh > apm_agent_install.sh && REGION=eu-west-0 bash apm_agent_install.sh -ak $ak -sk $sk -region $region -projectid $projectid -accessip 100.125.0.94 -obsdomain oss.eu-west-0.prod-cloud-ocb.orange-business.com"
+
+# Run the command
+$command
