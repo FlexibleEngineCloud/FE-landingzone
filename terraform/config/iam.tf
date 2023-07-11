6ec4859569c19ba9637ad611a4a8e9a04904673b
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-07-11 19:14:55 +0000

    Merge iam modules in one file

---
+++ terraform/config/iam.tf
@@ -0,0 +1,52 @@
+# Provision projects
+module "iam_projects" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/project"
+
+  project_names = local.project_names
+  region        = var.region
+}
+
+# Provision User Groups
+module "iam_groups" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/user_group"
+
+  group_names = local.group_names
+}
+
+# Provision Group Memberships
+module "group_membership" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/group_membership"
+
+  group_membership = local.group_membership
+
+  depends_on = [
+    module.iam_groups
+  ]
+}
+
+# Assinging Roles module
+module "iam_roles" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+
+  role_assignments = local.role_assignments
+
+  depends_on = [
+    module.group_membership
+  ]
+}
