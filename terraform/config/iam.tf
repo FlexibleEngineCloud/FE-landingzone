7e9b64b29a2a45cf79c6d0091876807e64fc2a58
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-09-08 13:54:15 +0000

    Add projects json file, projects need to be pre-created

--- terraform/config/iam.tf
+++ terraform/config/iam.tf
@@ -1,15 +1,3 @@
-# Provision projects
-module "iam_projects" {
-  providers = {
-    flexibleengine = flexibleengine.home_fe
-  }
-
-  source = "../modules/iam/project"
-
-  project_names = local.project_names
-  region        = var.region
-}
-
 # Provision User Groups
 module "iam_groups" {
   providers = {
