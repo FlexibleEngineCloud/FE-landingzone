d830ed5215827772faabcc951d55c8ffd22f35f8
Abd Elmoumen DRICI <abdelmoumendrici@gmail.com>
2023-09-14 15:42:34 +0000

    Update IAM policies/roles attached on projects and groups

--- terraform/config/iam.tf
+++ terraform/config/iam.tf
@@ -38,3 +38,219 @@ module "iam_roles" {
     module.group_membership
   ]
 }
+
+# Provision IAM Policy that grants enhanced read access on all OBS buckets.
+module "OBS_bucket_viewer_enhanced_role" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role"
+
+  roles = [{
+    name              = "obs-bucket-viewer-enhanced"
+    description       = "Read access on OBS"
+    type              = "AX"
+    policy      = <<EOF
+        {
+        "Version": "1.1",
+        "Statement": [
+                {
+                    "Action": [
+                        "OBS:bucket:Get*",
+                        "OBS:bucket:ListAllMyBuckets",
+                        "OBS:bucket:HeadBucket"
+                    ],
+                    "Resource": [
+                        "OBS:*:*:bucket:*",
+                        "OBS:*:*:object:*"
+                    ],
+                    "Effect": "Allow"
+                }
+            ]
+        }
+        EOF
+   }
+   ]
+}
+
+module "OBS_bucket_viewer_role_assignment" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["domain_admin"]
+        domain_id = var.domain_id
+        role_id    = module.OBS_bucket_viewer_enhanced_role.id[0]
+    },
+    {
+        group_id   = local.group_ids["network_admin"]
+        domain_id = var.domain_id
+        role_id    = module.OBS_bucket_viewer_enhanced_role.id[0]
+    },
+    {
+        group_id   = local.group_ids["dev_admin"]
+        domain_id = var.domain_id
+        role_id    = module.OBS_bucket_viewer_enhanced_role.id[0]
+    },
+    {
+        group_id   = local.group_ids["preprod_admin"]
+        domain_id = var.domain_id
+        role_id    = module.OBS_bucket_viewer_enhanced_role.id[0]
+    },
+    {
+        group_id   = local.group_ids["prod_admin"]
+        domain_id = var.domain_id
+        role_id    = module.OBS_bucket_viewer_enhanced_role.id[0]
+    }
+    ]
+}
+
+# Assinging 'FullAccess' and 'OBS FullAccess' to super_admin
+module "fullaccess_role_assignment" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["super_admin"]
+        domain_id = var.domain_id
+        role_id    = "9287ee3e297a41249ef85500a2f4e7d4"
+    },
+    {
+        group_id   = local.group_ids["super_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
+
+# Assinging 'Security Administrator' role to security_admin
+module "global_role_assignment_security" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["security_admin"]
+        domain_id = var.domain_id
+        role_id    = "4b6459d7a68f45f0b3cf7682c21c57e2"
+    }]
+}
+
+
+# Assinging 'Global' roles to domain_admin
+# 'TMS admin', 'IAM RO', 'OBS Full Accces'
+module "global_role_assignment_domain" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["domain_admin"]
+        domain_id = var.domain_id
+        role_id    = "0cd3367cdca345a7aa8607b9d1645232"
+    },
+    {
+        group_id   = local.group_ids["domain_admin"]
+        domain_id = var.domain_id
+        role_id    = "51e89a12ca524205addb639ca85adcc8"
+    },
+    {
+        group_id   = local.group_ids["domain_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
+
+# Assinging 'Global' roles to network_admin
+# 'IAM RO', 'OBS Full Accces'
+module "global_role_assignment_network" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["network_admin"]
+        domain_id = var.domain_id
+        role_id    = "51e89a12ca524205addb639ca85adcc8"
+    },
+    {
+        group_id   = local.group_ids["network_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
+
+# Assinging 'Global' roles to dev_admin
+# 'IAM RO', 'OBS Full Accces'
+module "global_role_assignment_dev" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["dev_admin"]
+        domain_id = var.domain_id
+        role_id    = "51e89a12ca524205addb639ca85adcc8"
+    },
+    {
+        group_id   = local.group_ids["dev_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
+
+# Assinging 'Global' roles to preprod_admin
+# 'IAM RO', 'OBS Full Accces'
+module "global_role_assignment_preprod" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["preprod_admin"]
+        domain_id = var.domain_id
+        role_id    = "51e89a12ca524205addb639ca85adcc8"
+    },
+    {
+        group_id   = local.group_ids["preprod_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
+
+# Assinging 'Global' roles to prod_admin
+# 'TMS admin', 'IAM RO', 'OBS Full Accces'
+module "global_role_assignment_prod" {
+  providers = {
+    flexibleengine = flexibleengine.home_fe
+  }
+
+  source = "../modules/iam/role_assignment"
+  role_assignments = [
+    {
+        group_id   = local.group_ids["prod_admin"]
+        domain_id = var.domain_id
+        role_id    = "51e89a12ca524205addb639ca85adcc8"
+    },
+    {
+        group_id   = local.group_ids["prod_admin"]
+        domain_id = var.domain_id
+        role_id    = "7e573fea4da7427d8ca3d41e699290d7"
+    }]
+}
--- terraform/config/iam_outputs.tf
+++ terraform/config/iam_outputs.tf
@@ -1,8 +1,3 @@
-# Project IDs as output
-output "project_ids" {
-  value = local.project_ids
-}
-
 # Group IDs as output
 output "groups_ids" {
   value = local.group_ids
@@ -16,4 +11,44 @@ output "group_membership" {
 # Role Assignments as output
 output "role_assignments" {
   value = local.role_assignments
+}
+
+# Output for "OBS_bucket_manager_role_assignment" module
+output "OBS_bucket_manager_role_assignments" {
+  value = module.OBS_bucket_viewer_role_assignment.role_assignments
+}
+
+# Output for "fullaccess_role_assignment" module
+output "fullaccess_role_assignments" {
+  value = module.fullaccess_role_assignment.role_assignments
+}
+
+# Output for "global_role_assignment_security" module
+output "global_role_assignment_security_assignments" {
+  value = module.global_role_assignment_security.role_assignments
+}
+
+# Output for "global_role_assignment_domain" module
+output "global_role_assignment_domain_assignments" {
+  value = module.global_role_assignment_domain.role_assignments
+}
+
+# Output for "global_role_assignment_network" module
+output "global_role_assignment_network_assignments" {
+  value = module.global_role_assignment_network.role_assignments
+}
+
+# Output for "global_role_assignment_dev" module
+output "global_role_assignment_dev_assignments" {
+  value = module.global_role_assignment_dev.role_assignments
+}
+
+# Output for "global_role_assignment_preprod" module
+output "global_role_assignment_preprod_assignments" {
+  value = module.global_role_assignment_preprod.role_assignments
+}
+
+# Output for "global_role_assignment_prod" module
+output "global_role_assignment_prod_assignments" {
+  value = module.global_role_assignment_prod.role_assignments
 }
